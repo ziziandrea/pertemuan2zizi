d0b24968ef3f9841c9933c4bb4f1910548364d5b
zizi andrea <ziziandrea@gmail.com>
2022-03-17 14:56:17 +0700

    pertemuan 2 zizi andrea Fungsi tanpa nilai kembali

--- src/main/kotlin/FungsiTanpaNilaiKembali.kt
+++ src/main/kotlin/FungsiTanpaNilaiKembali.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    displayMessage("Rekayasa Perangkat Lunak", 10);
+    displayMessage("D4 Rekayasa Perangkat Lunak", 10);
 }
 fun displayMessage(msg: String, count: Int) {
     var counter = 1
