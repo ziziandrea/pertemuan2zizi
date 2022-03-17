670482ef22bfd172b9f6b4e9966b2087dff0399f
zizi andrea <ziziandrea@gmail.com>
2022-03-17 09:39:25 +0700

    pertemuan 2 zizi andrea Array Primitive

---
+++ src/main/kotlin/ArrayPrimitive.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    val x: IntArray = intArrayOf(1, 2, 3)
+    x[0] = x[1] + x[2]
+    x.forEach { print(it.toString() + ' ')}
+    print('\n')
+
+    //Array of int of size 5 with values [0, 0, 0, 0, 0]
+    val byteArray = ByteArray(5)
+    byteArray.forEach { print(it.toString() + ' ')}
+    print('\n')
+
+    val shortArray = ShortArray(5) { 42 }
+    shortArray.forEach { print(it.toString() + ' ') }
+    print('\n')
+
+    var intArray =  IntArray(5) { it + 1}
+    intArray.forEach { print(it.toString() + ' ') }
+    print('\n')
+
+}
