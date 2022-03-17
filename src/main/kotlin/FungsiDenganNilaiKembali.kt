19fdf12349f879653cf8e525b76633c22085694e
zizi andrea <ziziandrea@gmail.com>
2022-03-17 15:00:51 +0700

    pertemuan 2 zizi andrea Fungsi dengan nilai kembali

---
+++ src/main/kotlin/FungsiDenganNilaiKembali.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val listNumber = listOf(1,2,3,4,5,6)
+    println("Jumlah nilai pada elemen listnumber = ${getSum(listNumber)}")
+}
+
+fun getSum(values: List<Int>) : Int {
+    var total = 0;
+    for (i in values) total += i
+    return total
+}
