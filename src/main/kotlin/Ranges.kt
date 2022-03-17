b9b9b7e0d44ac26f8f12ae56c91d80b3057267c9
zizi andrea <ziziandrea@gmail.com>
2022-03-17 11:14:02 +0700

    pertemuan 2 zizi andrea Range

---
+++ src/main/kotlin/Ranges.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    //Ranges
+    val i:Int = 2
+    for (j in 1..4)
+        print(j) // prints "1234"
+    println()
+
+    if (i in 1..10) {
+        println("we found your number --"+i)
+    }
+}
